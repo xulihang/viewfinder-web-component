6f24fd46b104a2b7c3f1403576c5c561e243c3db
xulihang <xulihanghai@163.com>
2022-07-20 17:33:15 +0800

    add host parent

--- src/components/view-finder/view-finder.tsx
+++ src/components/view-finder/view-finder.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, h, Host, Prop } from '@stencil/core';
 
 export interface ScanRegion {
   left:number;
@@ -19,39 +19,41 @@ export class ViewFinder {
   @Prop() preserveAspectRatio?: string;
   render() {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" 
-      preserveAspectRatio={this.preserveAspectRatio}
-      viewBox={"0 0 "+this.width+" "+this.height}>
-        <defs>
-          <mask id="myMask">
-            <rect 
-              x="0" 
-              y="0" 
-              width={this.width} 
-              height={this.height} fill="white" />
-            <rect 
-              x={this.scanRegion.left} 
-              y={this.scanRegion.top} 
-              width={this.scanRegion.width} 
-              height={this.scanRegion.height} fill="black" 
-            />
-          </mask>
-        </defs>
-        
-        <line 
-          x1={this.scanRegion.left}
-          y1={this.scanRegion.top}
-          x2={this.scanRegion.left+this.scanRegion.width}
-          y2={this.scanRegion.top} 
-          class="scan-line">
-          <animate attributeName="y1" to={this.scanRegion.top+this.scanRegion.height}  begin="0s" dur="2s"  repeatCount="indefinite" />
-          <animate attributeName="y2" to={this.scanRegion.top+this.scanRegion.height}  begin="0s" dur="2s"  repeatCount="indefinite" />
-        </line>
-        <rect width={this.width} height={this.height} class="mask-rect" mask="url(#myMask)">
-        </rect>
-        <rect x={this.scanRegion.left} y={this.scanRegion.top} width={this.scanRegion.width} height={this.scanRegion.height} class="scan-rect"/>
-
-      </svg>
+      <Host>
+        <svg xmlns="http://www.w3.org/2000/svg" 
+          preserveAspectRatio={this.preserveAspectRatio}
+          viewBox={"0 0 "+this.width+" "+this.height}>
+          <defs>
+            <mask id="myMask">
+              <rect 
+                x="0" 
+                y="0" 
+                width={this.width} 
+                height={this.height} fill="white" />
+              <rect 
+                x={this.scanRegion.left} 
+                y={this.scanRegion.top} 
+                width={this.scanRegion.width} 
+                height={this.scanRegion.height} fill="black" 
+              />
+            </mask>
+          </defs>
+          
+          <line 
+            x1={this.scanRegion.left}
+            y1={this.scanRegion.top}
+            x2={this.scanRegion.left+this.scanRegion.width}
+            y2={this.scanRegion.top} 
+            class="scan-line">
+            <animate attributeName="y1" to={this.scanRegion.top+this.scanRegion.height}  begin="0s" dur="2s"  repeatCount="indefinite" />
+            <animate attributeName="y2" to={this.scanRegion.top+this.scanRegion.height}  begin="0s" dur="2s"  repeatCount="indefinite" />
+          </line>
+          <rect width={this.width} height={this.height} class="mask-rect" mask="url(#myMask)">
+          </rect>
+          <rect x={this.scanRegion.left} y={this.scanRegion.top} width={this.scanRegion.width} height={this.scanRegion.height} class="scan-rect"/>
+        </svg>
+      </Host>
+      
     );
   }
 
